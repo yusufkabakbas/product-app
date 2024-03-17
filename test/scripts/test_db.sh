1e5bcc25495e0e8a5f302830b76d8e1de1ae024e
ykabakbas <yusufkabakbas@icloud.com>
2024-03-17 19:51:00 +0300

    created test_db.sh

--- test/scripts/test_db.sh
+++ test/scripts/test_db.sh
@@ -1,1 +1,21 @@
 #!/bin/zsh
+
+docker run --name postgres-test -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -p 6432:5432 -d postgres:latest
+echo "Postgresql starting..."
+sleep 3
+
+docker exec -it postgres-test psql -U postgres -d postgres -c "CREATE DATABASE productapp"
+sleep 3
+echo "Database productapp created"
+docker exec -it postgres-test psql -U postgres -d productapp -c "
+create table if not exists products
+(
+id bigserial not null primary key,
+name varchar(300) not null,
+price double precision not null,
+discount double precision,
+store varchar(300) not null
+);
+"
+sleep 3
+echo "Table products created"
